16106c5245b78e0be004488bec0e61015aedd527
Target <142077321+Target-blackbox@users.noreply.github.com>
2024-09-30 15:24:55 +0000

    1st class basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const account_id = 1445
+let account_email = "xyz@gmail.com"
+var account_pass = "12345"
+account_city = "hyderabad"
+let account_state;
+
+// account_id = 2  not allowed 
+
+account_email = "aiugfk@askdh.com"
+account_pass = "gfasdfdu2328"
+account_city = "delhi"
+/*
+dont use var beacuse of bloack scope.
+*/
+
+console.log(account_id);
+console.table([account_id,account_email,account_pass,account_city,account_state])
