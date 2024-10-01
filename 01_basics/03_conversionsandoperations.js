0874db0fe6c2984a1d5596ff5c0ec5ffb3f658c2
Target <142077321+Target-blackbox@users.noreply.github.com>
2024-10-01 14:13:13 +0000

    conversions of datatypes

---
+++ 01_basics/03_conversionsandoperations.js
@@ -0,0 +1,27 @@
+let score = "33"
+
+console.log(typeof score);
+
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber);
+
+// "33" => 33
+//"33abc" => gives NaN (notanumber)
+//"true" gives 1 false 0
+
+let is_logged_in = ""
+let booleanisloggedin = Boolean(is_logged_in)
+console.log(typeof booleanisloggedin);
+console.log(booleanisloggedin);
+
+// 1 gives true 0 gives false
+// "" gives false
+// string ex: "akhil" gives true
+
+let number = 33
+let string = String(number)
+console.log(string);
+
+console.log(typeof string);
+
