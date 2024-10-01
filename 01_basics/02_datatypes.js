f282f19eec96df52e4515df78b03f22d704a9435
Target <142077321+Target-blackbox@users.noreply.github.com>
2024-10-01 13:59:17 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict" //treat all js code as newer version
+
+//alert(3+3) we are using node js not browserl
+
+console.log(3+3); //code readability should be high
+
+
+let name = "akhil"
+let age = 3;
+let is_logged_in = false
+
+//primitive data type
+
+// number => 2 to pwer 53
+// bigint 
+// string prefer double ""
+//boolean true/false
+// null => stand alone value is an object
+// undefined 
+// symbol 
+
+//object
+console.log(typeof undefined); // output is always undefined
+console.log(typeof null); //output is null  
+
+
+
